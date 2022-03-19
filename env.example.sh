863a71be821741f8107684f9325dd0d8ccbe9fe8
ethicnology <ethicnology@pm.me>
2022-03-20 00:27:45 +0100

    fix: replace dotenv by linux env feat: bridge json-rpc requests

---
+++ env.example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export HOST= #value: 127.0.0.1:1984
+export SSL_KEY= #value: /path/to/key.pem
+export SSL_CRT= #value: /path/to/fullchain.pem
+export RPC_HOST= #value: http://127.0.0.1:18332
+export RPC_USER= #value: user
+export RPC_PASS= #value: pass
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,61 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{get, post, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-#[macro_use]
-extern crate dotenv_codegen;
+use reqwest::*;
+use serde_json::{from_str, json, Value};
+use std::env;
 
-async fn index(_req: HttpRequest) -> impl Responder {
-    "Wesh"
+#[get("/")]
+async fn index(body: HttpRequest) -> impl Responder {
+    println!("{:?}", body);
+    let a = json!({
+        "jsonrpc": "1.0",
+        "id": "test",
+        "method": "getnetworkinfo",
+        "params": [],
+    });
+    HttpResponse::Ok().body(bridge(a).await.unwrap())
+}
+
+#[post("/")]
+async fn echo(body: String) -> impl Responder {
+    println!("{:?}", body);
+    let a = bridge(from_str(&body).unwrap()).await.unwrap();
+    HttpResponse::Ok().body(a)
+}
+
+async fn bridge(body: Value) -> Result<String> {
+    let username = env::var("RPC_USER").unwrap();
+    let password = env::var("RPC_PASS").unwrap();
+    let client = reqwest::Client::new();
+    let response = client
+        .post(env::var("RPC_HOST").unwrap())
+        .basic_auth(username, Some(password))
+        .json(&body)
+        .send()
+        .await
+        .unwrap();
+    let text = response.text().await.unwrap();
+    println!("{:?}", text);
+    Ok(text)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!(
+        "https://{} {} {}",
+        env::var("HOST").unwrap(),
+        env::var("SSL_KEY").unwrap(),
+        env::var("SSL_CRT").unwrap()
+    );
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     builder
-        .set_private_key_file(dotenv!("SSL_KEY"), SslFiletype::PEM)
+        .set_private_key_file(env::var("SSL_KEY").unwrap(), SslFiletype::PEM)
         .unwrap();
     builder
-        .set_certificate_chain_file(dotenv!("SSL_CRT"))
+        .set_certificate_chain_file(env::var("SSL_CRT").unwrap())
         .unwrap();
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind_openssl((dotenv!("IP"), dotenv!("PORT")), builder)?
+    HttpServer::new(|| App::new().service(index).service(echo))
+        .bind_openssl(env::var("HOST").unwrap(), builder)?
         .run()
         .await
 }

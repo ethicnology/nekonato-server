01e6450218b05efe3c24d9cfef4ed3333f076835
ethicnology <ethicnology@pm.me>
2022-03-19 14:27:02 +0100

    feat: add actix4 openssl template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+
+async fn index(_req: HttpRequest) -> impl Responder {
+    "Wesh"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind_openssl("127.0.0.1:8080", builder)?
+        .run()
+        .await
 }
